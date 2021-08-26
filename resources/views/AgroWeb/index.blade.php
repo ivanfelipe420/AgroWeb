bb02e6f6ff887ad461398f9d83cb451b2ee40ac3
ivanfelipe420 <Pglcannabis420@gmail.com>
2021-08-26 13:01:35 -0500

    versionimg

--- resources/views/AgroWeb/index.blade.php
+++ resources/views/AgroWeb/index.blade.php
@@ -7,13 +7,44 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
     crossorigin="anonymous">
-    <link rel="shortcun icon" type="imagen/x-icon" href="Public/agroWebFavicon.ico">
+    <link rel="shortcun icon" type="imagen/x-icon" href="/Imagenes/logos.ico">
+    <link rel="stylesheet" href="/css/app.css">
     <title>AgroWeb</title>
 </head>
 <body>
+@if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    
+<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorias
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/Imagenes/logo.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      AgroWeb
+    </a>
+  </div>
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -25,17 +56,7 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Categorias
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
+        
         <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
@@ -47,5 +68,11 @@
     </div>
   </div>
 </nav>
+<div class="banner">
+	    <div class="capa">
+	        <img src="agro.png"  width="150" height="150" >
+            <img src="letra.pgn">
+		</div>
+	</div>
 </body>
 </html>
