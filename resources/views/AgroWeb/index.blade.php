97519291a6346afdb87ec63378b75ea282acd87d
ivanfelipe420 <Pglcannabis420@gmail.com>
2021-09-01 09:03:05 -0500

    version6

--- resources/views/AgroWeb/index.blade.php
+++ resources/views/AgroWeb/index.blade.php
@@ -8,10 +8,11 @@
     integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" 
     crossorigin="anonymous">
     <link rel="shortcun icon" type="imagen/x-icon" href="/Imagenes/logos.ico">
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="estilo.css">
     <title>AgroWeb</title>
 </head>
 <body>
+    
 @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -40,7 +41,7 @@
         </li>
 <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="/Imagenes/logo.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      <img src="/Imagenes/logos.ico" alt="" width="30" height="24" class="d-inline-block align-text-top">
       AgroWeb
     </a>
   </div>
@@ -68,11 +69,42 @@
     </div>
   </div>
 </nav>
-<div class="banner">
-	    <div class="capa">
-	        <img src="agro.png"  width="150" height="150" >
-            <img src="letra.pgn">
-		</div>
-	</div>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+        </div>
+        <div class="carousel-inner" style="width: 90%;margin:0 auto">
+          <div class="carousel-item active">
+            <img src="public/Imagenes/banner1.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="public/Imagenes/banner2.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="public/Imagenes/banner3.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="public/Imagenes/banner4.png" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="public/Imagenes/banner5.png" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 </html>
