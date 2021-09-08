496aaf451076df1ef25244724ebcdf5553837872
ivanfelipe420 <Pglcannabis420@gmail.com>
2021-09-08 09:50:24 -0500

    versionTabla

---
+++ database/migrations/2021_09_08_144728_create_table__productos.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProductos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Productos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Productos');
+    }
+}
