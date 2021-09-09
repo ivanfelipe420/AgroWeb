608cc4cdb685d04cd9a6687636a484c4189d42f7
Yessica <cardenas19yessica@gmail.com>
2021-09-09 11:34:49 -0500

    9/9/11:34am

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,7 +89,7 @@
                                 </a>
 
                                 
-
+                                
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/">
                                         Mi cuenta
