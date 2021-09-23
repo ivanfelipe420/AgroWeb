ace7301efcb7a67a074a5ea5c62b05f0e8520b8f
Alexander.Garcia6 <alex199-6@outlook.com>
2021-09-23 12:58:01 -0500

    Alexander.32/sep-12:57

--- storage/framework/views/25978298977b18ecc6e9ec9ceede2e77c46ef296.php
+++ storage/framework/views/25978298977b18ecc6e9ec9ceede2e77c46ef296.php
@@ -84,7 +84,7 @@
                         <?php else: ?>
                             <li class="nav-item dropdown">
                                 
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="/cuenta" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     <?php echo e(Auth::user()->name); ?>
 
                                 </a>
@@ -229,7 +229,133 @@
            <img src="/Imagenes/menu.svg" alt=""></button>
       </form>
 <!--carrusel-->
+<!-- Footer -->
+<div  >
+<footer class="contac">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span><center><strong>Contactos:</strong></center></span>
+    </div>
+    <!-- Left -->
 
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Advertencia
+          </h6>
+          <p>
+            esta pagina garantiza la comunicacion entre el cliente y el vendedor, no nos hacemos responsables de la entrega del producto ni su garantia. eso le corresponde al establecimiento o al vendedor solicitado .
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+           FRUVERS
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Canasta campesina-madrid</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">camacho-Funza</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">La Gran Cocecha-madrid</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Corabastos-facatativa</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          Telefono
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">+57 314 567 8832</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">+57 312 367 8832</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">+57 301 557 8832</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">+57 310 567 8832</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+              Contact
+          </h6>
+          <p><i class="fas fa-home me-3"></i>Madrid,Cundinmarca</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+          correofruvers@example.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i>+57 314 567 8832</p>
+          <p><i class="fas fa-print me-3"></i>+57 234 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">AgroWeb </a>
+  </div>
+  <!-- Copyright -->
+</footer>
+</div>
+<!-- Footer --> 
     
     <script  type="text/javascript" src="<?php echo e(asset('js/pushbar.js')); ?>"></script>
         <script src="Js/pushbar.js"></script>
--- storage/framework/views/39bc5c325611e42a08db365b016ff631c7173249.php
+++ storage/framework/views/39bc5c325611e42a08db365b016ff631c7173249.php
@@ -131,4 +131,4 @@
 
 <?php $__env->stopSection(); ?>
 
-<?php echo $__env->make('layouts.app2', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\AgroWeb\resources\views//home.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\AgroWeb\resources\views//home.blade.php ENDPATH**/ ?>
