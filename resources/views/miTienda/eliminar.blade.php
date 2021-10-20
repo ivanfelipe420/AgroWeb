958bc17951778f5d40500d1ad76e814ef55e13ed
JuanMoreno003 <jusemoar003@gmail.com>
2021-10-20 13:43:33 -0500

    20/10/2021 1:44

--- resources/views/miTienda/eliminar.blade.php
+++ resources/views/miTienda/eliminar.blade.php
@@ -5,7 +5,6 @@
 <div class="container">
     <div class=row>
         <div class=col>
-<<<<<<< HEAD
             <h2>{{Auth::user()->name}}, se eliminará tu Tienda {{$eliminarT->nombreTienda}} permanentemente. ¿Estas segur@?</h2>
             <form action="/miTienda/{{Auth::user()->id}}" method="POST">
                 @csrf   
@@ -17,27 +16,4 @@
             <a href="{{ url()->previous() }}" class="btn btn-outline-success" disabled>Atras</a>
     </div>
 </div>
-@endsection 
-=======
-            <h2>Eliminar Mi Tienda:</h2>
-                    <!-- Formulario -->     
-                <form action="/categorias" method="post">
-                        @csrf
-                        @method('delete')
-                        <div class="form-group">
-                        <h2>{{Auth::user()->name}}, se eliminará tu Tienda {{$eliminarT->nombreTienda}} permanentemente. ¿Estas segur@?</h2>
-                                <form action="/miTienda/{{Auth::user()->id}}" method="POST">
-                                @csrf   
-                                @method('delete')
-                                <input type="hidden"class="form-control" id="idEliminar" name="idEliminar"  value="{{$eliminarT->id}}" >
-                                <button href="/home" class="btn btn-outline-danger">Borrar</button><br><br>
-                            </form>
-
-            <a href="{{ url()->previous() }}" class="btn btn-outline-success" disabled>Atras</a>
-        </div><br>
-        
-    </div>
-</div>
-
-@endsection
->>>>>>> 9b33cbcd6d1881c3aeb87487b90744275b633360
+@endsection  
