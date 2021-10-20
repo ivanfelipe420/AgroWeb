c48020c4e7a52d5776368e9cf08a7d7ecfbc202b
ivanfelipe420 <Pglcannabis420@gmail.com>
2021-10-20 12:21:03 -0500

    Ivan_20/12:20

--- resources/views/cuenta/edit.blade.php
+++ resources/views/cuenta/edit.blade.php
@@ -2,31 +2,37 @@
 @section('url', __('/home'))
 
 @section('content') 
-<div class="container" style="background:rgb(240, 240, 193); border-radius:5px; padding:10px;">
+<!--opciones-->
+<div class="container">
     <div class="row">
         <div class="col-2">
-            <a href="/cuenta" class="btn btn-outline-success" disabled>Mi cuenta</a><br><br>
-            <a href="/productos" class="btn btn-outline-success" disabled>Mi tienda</a>
+            <li type="" class="list-group-item list-group-item-action" disabled="disabled">
+                <a href="#" class="list-group-item list-group-item-action" disabled>Mi Tienda</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="/cuenta" class="list-group-item list-group-item-action" disabled>Mi Cuenta</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="" class="list-group-item list-group-item-action" disabled>Editar Tienda</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="" class="list-group-item list-group-item-action" disabled>Eliminar Tienda</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="/miTienda/{{Auth::user()->id}}/miTienda" class="list-group-item list-group-item-action" disabled>¡Ir a mi tienda!</a></li>
         </div>
         <div class="col-10">
             <h2>Editar perfil:</h2>
-                            <form action="/cuenta/{{Auth::user()->id}}" method="POST" enctype="multipart/form-data">
-                            @csrf   
-                            @method('put')
-                                <div class="form-group">
-                                    <label for="Nombre">Digite nombre</label>
-                                    <input type="text" class="form-control" id="cajaNombre" name="cajaNombre" value="{{Auth::user()->name}}"required>
-                                    <br>
-                                    <label for="telefono">Digite telefono</label>
-                                    <input type="text" class="form-control" id="cajaTelefono" name="cajaTelefono" value="{{Auth::user()->telefono}}"required>
-                                </div>
-                                <button class="btn btn-lg btn-primary" type="submit">Editar</button>
-                            </form>
-                            <br>
+                <form action="/cuenta/{{Auth::user()->id}}" method="POST" enctype="multipart/form-data">
+                    @csrf   
+                        @method('put')
+                        <div class="form-group">
+                            <label for="Nombre">Digite nombre</label>
+                            <input type="text" class="form-control" id="cajaNombre" name="cajaNombre" value="{{Auth::user()->name}}">
+                                <br>
+                            <label for="telefono">Digite telefono</label>
+                            <input type="text" class="form-control" id="cajaTelefono" name="cajaTelefono" value="{{Auth::user()->telefono}}">
                         </div>
-                    </div>
-                    <a href="{{ url()->previous() }}" class="btn btn-outline-success" disabled>Atras</a>
-        
+                        <br>
+                    <button class="btn btn-lg btn-primary" type="submit">Editar</button>
+                </form>
+            <br>
         </div>
     </div>  
 </div>
--- resources/views/miTienda/infoTienda.blade.php
+++ resources/views/miTienda/infoTienda.blade.php
@@ -14,28 +14,27 @@
 <div class="container">
     <div class="row">
         <div class="col-2">
-        <li type="" class="list-group-item list-group-item-action" disabled="disabled">
-            <a href="#" class="list-group-item list-group-item-action" disabled>Mi Tienda</a></li>
-        <li type="button" class="list-group-item list-group-item-action">
-            <a href="/cuenta" class="list-group-item list-group-item-action" disabled>Mi Cuenta</a></li>
-        <li type="button" class="list-group-item list-group-item-action">
-            <a href="" class="list-group-item list-group-item-action" disabled>Editar Tienda</a></li>
-        <li type="button" class="list-group-item list-group-item-action">
-            <a href="" class="list-group-item list-group-item-action" disabled>Eliminar Tienda</a></li>
-        <li type="button" class="list-group-item list-group-item-action">
-            <a href="/miTienda/{{Auth::user()->id}}/miTienda" class="list-group-item list-group-item-action" disabled>¡Ir a mi tienda!</a></li>
+            <li type="" class="list-group-item list-group-item-action" disabled="disabled">
+                <a href="#" class="list-group-item list-group-item-action" disabled>Mi Tienda</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="/cuenta" class="list-group-item list-group-item-action" disabled>Mi Cuenta</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="" class="list-group-item list-group-item-action" disabled>Editar Tienda</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="" class="list-group-item list-group-item-action" disabled>Eliminar Tienda</a></li>
+            <li type="button" class="list-group-item list-group-item-action">
+                <a href="/miTienda/{{Auth::user()->id}}/miTienda" class="list-group-item list-group-item-action" disabled>¡Ir a mi tienda!</a></li>
         </div>
         <div class="col-10">
             <h2>Nombre de mi tienda:</h2>
-            <li type="" class="list-group-item list-group-item-action" disabled="disabled">{{$tienda->nombreTienda}}</li>
+                <li type="" class="list-group-item list-group-item-action" disabled="disabled">{{$tienda->nombreTienda}}</li>
             <h2>Representante legal:</h2>
-            <h5>{{$tienda->nombrePropietario}}</h5>
+                <li type="" class="list-group-item list-group-item-action" disabled="disabled">{{$tienda->nombrePropietario}}</li>
             <h2>Dirección de tu tienda:</h2>
-            <h5>{{$tienda->direccion}}</h5>
+                <li type="" class="list-group-item list-group-item-action" disabled="disabled">{{$tienda->direccion}}</li>
             <h2>Informacion de contacto</h2>
-            <h5>Correo de tu tienda: {{$tienda->email}}</h5>
-            
-            <h5 id="telefono">Telefono de tu tienda: {{$tienda->telefono}}</h5>
+                <li type="" class="list-group-item list-group-item-action" disabled="disabled">Correo de tu tienda: {{$tienda->direccion}}</li>
+                <li id="telefono" type="" class="list-group-item list-group-item-action" disabled="disabled">Telefono de tu tienda: {{$tienda->telefono}}</li>
         </div>
     </div>  
 </div>
