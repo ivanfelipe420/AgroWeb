7e4bc3a27a77c70e42a2389a1f627725f19f1cda
Yerson21YY <ryersonromero@gmail.com>
2021-10-22 15:23:55 -0500

    Yeson22/10/21-3:23

--- resources/views/miTienda/index.blade.php
+++ resources/views/miTienda/index.blade.php
@@ -42,10 +42,6 @@ use App\Http\Controllers\tiendaControlador;
           }
       ?>
       
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
